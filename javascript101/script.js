cfd5e8e1039e6f0b69278c220f25aceed1c4bfa2
alsta010 <alsta010@osloskolen.no>
2026-08-27 21:57:26 +0200

    Final (Javascript 101)
    Final site to (Javascript 101)

--- javascript101/script.js
+++ javascript101/script.js
@@ -1,3 +1,54 @@
-function myFunction() {
-  alert("Hei");
+let count = 0;
+let count2 = 0;
+
+// Number Counter (Player 1)
+function updateCount() {
+    document.getElementById("count").innerHTML = count;
 }
+
+function increaseCount() {
+    count++;
+    updateCount();
+}
+
+function decreaseCount() {
+    count--;
+    updateCount();
+}
+
+function resetCount() {
+    count = 0;
+    updateCount();
+}
+
+// Number Counter (Player 2)
+function updateCount2() {
+    document.getElementById("count2").innerHTML = count2;
+}
+
+function increaseCount2() {
+    count2++;
+    updateCount2();
+}
+
+function decreaseCount2() {
+    count2--;
+    updateCount2();
+}
+
+function resetCount2() {
+    count2 = 0;
+    updateCount2();
+}
+
+// Alert System
+function myFunction() {
+  alert("Lain Iwakura: No matter where you are... everyone is always connected.");
+}
+
+// Dark Mode
+function toggle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
+
